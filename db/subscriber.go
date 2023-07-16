dfe4a582dc319804a19718bf173a81c311dbd4e1
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2023-07-16 13:34:42 +0200

    add: subscriber entity

---
+++ db/subscriber.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"gorm.io/gorm"
+)
+
+type Subscriber struct {
+	gorm.Model
+	ChatID  int `gorm:"<-:create"`
+	MensaID int
+	Push    PushTime `gorm:"embedded"`
+}
+
+type PushTime struct {
+	Hours   uint `gorm:"check:hours < 24"`   // 0 - 23
+	Minutes uint `gorm:"check:minutes < 60"` // 0 - 59
+}
+
+type SubscriberRepository struct {
+	db *gorm.DB
+}
+
+func NewSubscriberRepository(db *gorm.DB) SubscriberRepository {
+	return SubscriberRepository{
+		db: db,
+	}
+}
+
+func (repo SubscriberRepository) CreateSubscriber(chatID, mensaID int, pushHour, pushMinutes uint) (Subscriber, error) {
+	sub := Subscriber{ChatID: chatID, MensaID: mensaID, Push: PushTime{
+		Hours:   pushHour,
+		Minutes: pushMinutes,
+	}}
+	res := repo.db.Create(&sub)
+
+	return sub, res.Error
+}
+
+func (repo SubscriberRepository) FindSubscriberById(id uint) (Subscriber, error) {
+	var sub Subscriber
+	result := repo.db.First(&sub, id)
+	return sub, result.Error
+}
---
+++ db/subscriber_test.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var repo SubscriberRepository
+
+func TestMain(m *testing.M) {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = Migrate(db)
+	if err != nil {
+		panic(err)
+	}
+	repo = NewSubscriberRepository(db)
+	m.Run()
+}
+
+func TestCreateSubscriber(t *testing.T) {
+	sub, err := repo.CreateSubscriber(1, 2, 11, 30)
+	assert.Nil(t, err)
+	sub2, err := repo.CreateSubscriber(2, 3, 11, 31)
+	assert.Nil(t, err)
+	assert.NotEqual(t, sub.ID, sub2.ID)
+}
+
+func TestCreateAndRetrieveSubscriber(t *testing.T) {
+	sub, err := repo.CreateSubscriber(12, 13, 10, 00)
+	assert.Nil(t, err)
+
+	id := sub.ID
+	sub, err = repo.FindSubscriberById(id)
+	assert.Nil(t, err)
+
+	assert.Equal(t, id, sub.ID)
+	assert.Equal(t, 12, sub.ChatID)
+	assert.Equal(t, 13, sub.MensaID)
+	assert.Equal(t, uint(10), sub.Push.Hours)
+	assert.Equal(t, uint(0), sub.Push.Minutes)
+}
+
+func TestInsertWrongTime(t *testing.T) {
+	_, err := repo.CreateSubscriber(0, 0, 24, 0)
+	assert.NotNil(t, err)
+	_, err = repo.CreateSubscriber(0, 0, 0, 60)
+	assert.NotNil(t, err)
+}
