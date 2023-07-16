3d40e7e41cdd0ea26817fd0a5b16a18902967930
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2023-07-16 14:20:26 +0200

    add: scheduler

---
+++ domain/scheduler.go
@@ -0,0 +1,52 @@
+package domain
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Atgoogat/openmensarobot/db"
+	"github.com/go-co-op/gocron"
+)
+
+// Runs everytime a subscriber is scheduled according to its pushtime
+// parameter is the subscriber id
+// if an error is returned the subscriber is removed form the scheduler
+type SubscriberScheduleAction func(uint) error
+
+type SubscriberScheduler struct {
+	scheduler     *gocron.Scheduler
+	scheduledJobs map[uint]*gocron.Job
+	action        SubscriberScheduleAction
+}
+
+func NewSubscriberScheduler(action SubscriberScheduleAction) *SubscriberScheduler {
+	return &SubscriberScheduler{
+		scheduler:     gocron.NewScheduler(time.Local),
+		scheduledJobs: make(map[uint]*gocron.Job),
+		action:        action,
+	}
+}
+
+func (scheduler *SubscriberScheduler) InsertJob(subscriber db.Subscriber) error {
+	time := fmt.Sprintf("%2d:%2d", subscriber.Push.Hours, subscriber.Push.Minutes)
+	id := subscriber.ID
+	job, err := scheduler.scheduler.Every(1).Day().At(time).Do(func() {
+		if scheduler.action != nil {
+			err := scheduler.action(id)
+			if err != nil {
+				j := scheduler.scheduledJobs[id]
+				scheduler.scheduler.Remove(j)
+			}
+		}
+	})
+	if err != nil {
+		return err
+	}
+	scheduler.scheduledJobs[id] = job
+	return nil
+}
+
+func (scheduler *SubscriberScheduler) RemoveJob(id uint) {
+	job := scheduler.scheduledJobs[id]
+	scheduler.scheduler.Remove(job)
+}
--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require github.com/stretchr/testify v1.8.4
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-co-op/gocron v1.30.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/sqlite v1.5.2 // indirect
 	gorm.io/gorm v1.25.2 // indirect
